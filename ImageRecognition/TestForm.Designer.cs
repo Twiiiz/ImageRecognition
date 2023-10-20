79934abe8b6b9aa06887855240b9bc0936a4fc39
Twiiiz <tymur.mustaev@gmail.com>
2023-10-20 15:40:11 +0300

    Designed TestForm

--- ImageRecognition/TestForm.Designer.cs
+++ ImageRecognition/TestForm.Designer.cs
@@ -28,20 +28,272 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.radioButton3 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.ReturnButton = new System.Windows.Forms.Button();
+			this.ExitButton = new System.Windows.Forms.Button();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.TestButton = new System.Windows.Forms.Button();
+			this.groupBox2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
 			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(63, 84);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(133, 20);
+			this.textBox1.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+			this.label1.Location = new System.Drawing.Point(23, 66);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(209, 15);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Number of images per learning class";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(63, 127);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(133, 28);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Define";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(63, 194);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(133, 55);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "Choose a root folder for learning class";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(63, 279);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(133, 55);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "Choose a root folder for images you want to recognise";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(63, 420);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(133, 28);
+			this.button4.TabIndex = 7;
+			this.button4.Text = "Define";
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+			this.label2.Location = new System.Drawing.Point(38, 359);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(183, 15);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "Number of recognisable images";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(63, 377);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(133, 20);
+			this.textBox2.TabIndex = 5;
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.radioButton3);
+			this.groupBox2.Controls.Add(this.radioButton2);
+			this.groupBox2.Controls.Add(this.radioButton1);
+			this.groupBox2.Controls.Add(this.label3);
+			this.groupBox2.Controls.Add(this.label4);
+			this.groupBox2.Controls.Add(this.textBox3);
+			this.groupBox2.Location = new System.Drawing.Point(569, 47);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(361, 182);
+			this.groupBox2.TabIndex = 8;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Learning Parameters";
+			// 
+			// radioButton3
+			// 
+			this.radioButton3.AutoSize = true;
+			this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+			this.radioButton3.Location = new System.Drawing.Point(49, 128);
+			this.radioButton3.Name = "radioButton3";
+			this.radioButton3.Size = new System.Drawing.Size(60, 19);
+			this.radioButton3.TabIndex = 5;
+			this.radioButton3.TabStop = true;
+			this.radioButton3.Text = "10000";
+			this.radioButton3.UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.AutoSize = true;
+			this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+			this.radioButton2.Location = new System.Drawing.Point(49, 95);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(53, 19);
+			this.radioButton2.TabIndex = 4;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "5000";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.AutoSize = true;
+			this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+			this.radioButton1.Location = new System.Drawing.Point(49, 61);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(53, 19);
+			this.radioButton1.TabIndex = 3;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "1000";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+			this.label3.Location = new System.Drawing.Point(6, 32);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(196, 17);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Number of Learning Iterations";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label4.Location = new System.Drawing.Point(221, 72);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(134, 17);
+			this.label4.TabIndex = 1;
+			this.label4.Text = "Learning Coefficient";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(238, 92);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.TabIndex = 0;
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(569, 247);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(361, 108);
+			this.richTextBox1.TabIndex = 9;
+			this.richTextBox1.Text = "";
+			// 
+			// ReturnButton
+			// 
+			this.ReturnButton.Location = new System.Drawing.Point(683, 361);
+			this.ReturnButton.Name = "ReturnButton";
+			this.ReturnButton.Size = new System.Drawing.Size(163, 50);
+			this.ReturnButton.TabIndex = 10;
+			this.ReturnButton.Text = "Return to Start";
+			this.ReturnButton.UseVisualStyleBackColor = true;
+			// 
+			// ExitButton
+			// 
+			this.ExitButton.Location = new System.Drawing.Point(683, 421);
+			this.ExitButton.Name = "ExitButton";
+			this.ExitButton.Size = new System.Drawing.Size(163, 50);
+			this.ExitButton.TabIndex = 11;
+			this.ExitButton.Text = "Exit";
+			this.ExitButton.UseVisualStyleBackColor = true;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(264, 142);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(240, 306);
+			this.dataGridView1.TabIndex = 12;
+			// 
+			// TestButton
+			// 
+			this.TestButton.Location = new System.Drawing.Point(290, 84);
+			this.TestButton.Name = "TestButton";
+			this.TestButton.Size = new System.Drawing.Size(197, 43);
+			this.TestButton.TabIndex = 13;
+			this.TestButton.Text = "Test";
+			this.TestButton.UseVisualStyleBackColor = true;
 			// 
 			// TestForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(942, 483);
+			this.Controls.Add(this.TestButton);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.ExitButton);
+			this.Controls.Add(this.ReturnButton);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
 			this.Name = "TestForm";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "TestForm";
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.RadioButton radioButton3;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Button ReturnButton;
+		private System.Windows.Forms.Button ExitButton;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Button TestButton;
 	}
 }
