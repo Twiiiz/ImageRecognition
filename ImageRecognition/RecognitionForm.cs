3b290954f91e8761e2d0bb3b946065da1979c2ef
Twiiiz <tymur.mustaev@gmail.com>
2023-10-20 13:21:08 +0300

    Created all forms needed for application

--- ImageRecognition/RecognitionForm.cs
+++ ImageRecognition/RecognitionForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ImageRecognition
 {
-	public partial class Form1 : Form
+	public partial class RecognitionForm : Form
 	{
-		public Form1()
+		public RecognitionForm()
 		{
 			InitializeComponent();
 		}
--- ImageRecognition/StartForm.Designer.cs
+++ ImageRecognition/StartForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ImageRecognition
 {
-	partial class Form1
+	partial class StartForm
 	{
 		/// <summary>
 		/// Required designer variable.
